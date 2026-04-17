707f854ccf41623a96a5eebd2b57e578d50381a7
guta-demisu <gutademisu82991@gmail.com>
2026-04-17 09:44:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  // You can add AJAX code here for real submissions
+  alert('Message sent! (Demo)');
+});
